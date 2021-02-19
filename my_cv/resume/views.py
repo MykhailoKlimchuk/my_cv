5184c7f56e2ad5163e77a262c782c5478734a97e
38067 <mikeklimchuck@gmail.com>
2021-02-19 11:05:03 +0200

    update

--- my_cv/resume/views.py
+++ my_cv/resume/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
 
 # Create your views here.
+from django.views.generic.base import View
+from .models import PersonInfo
+
+
+class MainPageView(View):
+    def get(self, request):
+        person_info = PersonInfo.objects.all()
+        data = {
+
+        }
+        return render(request, "main_page.html", data)
+
