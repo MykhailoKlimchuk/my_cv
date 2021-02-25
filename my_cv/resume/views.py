53a03284ffb4937f23f92319bf28a7f292fa2177
38067 <mikeklimchuck@gmail.com>
2021-02-25 10:32:17 +0200

    update

--- my_cv/resume/views.py
+++ my_cv/resume/views.py
@@ -2,15 +2,21 @@ from django.shortcuts import render
 
 # Create your views here.
 from django.views.generic.base import View
-from .models import PersonInfo
+from .models import PersonInfo, Skill
 
 
 class MainPageView(View):
     def get(self, request):
         personal_info = PersonInfo.objects.all()[0]
+        print(personal_info)
+        print(Skill.objects.all())
+
+        skills = Skill.objects.all()
+
         data = {
-            "ggg": "11",
-            "personal_info": personal_info
+            "personal_info": personal_info,
+            "skills": skills,
         }
+
         return render(request, "main_page.html", data)
 
