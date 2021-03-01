3bdb5ff6b806841403f2ba66c98e1b6bdaf62468
38067 <mikeklimchuck@gmail.com>
2021-03-01 22:46:59 +0200

    update

--- my_cv/resume/views.py
+++ my_cv/resume/views.py
@@ -1,8 +1,9 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
 # Create your views here.
 from django.views.generic.base import View
 from .models import PersonInfo, Skill
+from .forms import ContactMeForm
 
 
 class MainPageView(View):
@@ -20,3 +21,24 @@ class MainPageView(View):
 
         return render(request, "main_page.html", data)
 
+class ContactMeView(View):
+    def get(self, request):
+        form = ContactMeForm()
+        return render(request, "contact_me/contact_me.html", {"form": form})
+
+    def post(self, request):
+        # todo добавить тг бота який буде адміну кидати сповіщення
+        form = ContactMeForm(request.POST)
+        error = ""
+        if form.is_valid():
+            form.save()
+            return redirect('home_page')
+        else:
+            error = "form is not valid"
+
+        data = {
+            "form": form,
+            "error": error,
+        }
+
+        return render(request, "contact_me/contact_me.html", data)
